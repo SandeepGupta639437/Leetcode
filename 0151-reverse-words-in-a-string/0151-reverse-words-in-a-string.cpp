58733c794fe56140ca224e36a3169360d2b100ca
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2025-01-24 02:14:45 +0530

    Time: 0 ms (100%), Space: 9.7 MB (97.15%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -3,17 +3,16 @@ public:
     string reverseWords(string s) {
         int n = s.size();
         reverse(s.begin(), s.end());
-
-        int l = 0, j = 0;
+        int i = 0, j = 0;
         while (j < n) {
             while (j < n && s[j] == ' ') j++;  
             if (j >= n) break;
-            if (l > 0) s[l++] = ' ';  
-            int start = l;
-            while (j < n && s[j] != ' ') s[l++] = s[j++]; 
-            reverse(s.begin() + start, s.begin() + l); 
+            if (i > 0) s[i++] = ' ';  
+            int start = i;
+            while (j < n && s[j] != ' ') s[i++] = s[j++]; 
+            reverse(s.begin() + start, s.begin() + i); 
         }
-        s.resize(l); 
+        s.resize(i); 
         return s;
     }
 };

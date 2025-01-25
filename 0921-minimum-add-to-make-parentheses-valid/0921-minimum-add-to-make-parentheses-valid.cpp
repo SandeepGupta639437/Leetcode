a8eeb3c8022baa999170915dff3182f547880dba
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2025-01-26 03:14:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int n=s.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='(')ans++;
+            if(s[i]==')')ans--;
+        }
+        return abs(ans);
+    }
+};

3767776cbe42a353cd46cf7e69badafb584c7123
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2025-02-19 15:16:50 +0530

    Time: 1021 ms (15.61%), Space: 98.2 MB (5.18%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
@@ -1,26 +1,23 @@
 class ProductOfNumbers {
-    vector<int> stream = {1};
-    int n;
-    int last_zero_idx;
 public:
+    vector<int>nums;
     ProductOfNumbers() {
-        n=1;
-        last_zero_idx=-1;
+        // nums.clear();
     }
     
     void add(int num) {
-        if(num==0)  last_zero_idx=n;
-
-        if(stream.back()==0)    stream.push_back(num);
-        else                    stream.push_back(stream.back()*num);
-        n++;
+        nums.push_back(num);
     }
     
     int getProduct(int k) {
-        if(last_zero_idx >= n-k)
-            return 0;
-        if(stream[n-k-1]==0)    return stream[n-1];
-        else                    return stream[n-1]/stream[n-k-1];
+       if (k > nums.size()) return 0;
+        
+        int product = 1;
+        for (int i = nums.size() - k; i < nums.size(); i++) {
+            product *= nums[i];
+        }
+        return product;
+ 
     }
 };
 

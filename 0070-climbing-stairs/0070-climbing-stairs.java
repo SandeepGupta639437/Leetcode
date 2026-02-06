394e06770a7f94594c5669282e9b838ed3a6a6c9
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-02-06 11:59:40 +0530

    Time: 0 ms (100%), Space: 42.2 MB (50.28%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,16 @@
 class Solution {
-    public int climbStairs(int n) {
+    int dp[] = new int[46];
 
-        if (n==1)return 1;
-        int []dp=new int[n+1];
-        dp[1]=1;
-        dp[2]=2;
-        for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+    int solve(int n) {
+        if (n == 1) return 1;
+        if (n == 0) return 1;   // Important: ways to stay at 0 = 1
+
+        if (dp[n] != 0) return dp[n];   // memo check
+
+        return dp[n] = solve(n - 1) + solve(n - 2);
+    }
+
+    public int climbStairs(int n) {
+        return solve(n);
     }
 }

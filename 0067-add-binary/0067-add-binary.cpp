b34c2ca53b631df9f085ad264d3e6788e040a03c
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-02-15 10:43:47 +0530

    Time: 0 ms (100%), Space: 9.1 MB (36.83%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,23 +1,89 @@
 class Solution {
- public:
-  string addBinary(string a, string b) {
-    string ans;
-    int carry = 0;
-    int i = a.length() - 1;
-    int j = b.length() - 1;
+public:
+    string addBinary(string a, string b) {
+        int i = a.size() - 1, j = b.size() - 1;
+        int carry = 0;
+        string c = "";
 
-    while (i >= 0 || j >= 0 || carry) {
-      if (i >= 0){
-        carry += a[i--] - '0';
-      }
-      if (j >= 0){
-        carry += b[j--] - '0';
-      }
-        ans += carry % 2 + '0';
-        carry /= 2;
+        while(i >= 0 || j >= 0 || carry){
+
+            if(i >= 0 && j >= 0){
+
+                if(a[i]=='1' && b[j]=='1' && carry){        // 111
+                    c += '1';
+                    carry = 1;
+                }
+                else if(a[i]=='1' && b[j]=='1' && !carry){  // 110
+                    c += '0';
+                    carry = 1;
+                }
+                else if(a[i]=='1' && b[j]=='0' && carry){   // 101
+                    c += '0';
+                    carry = 1;
+                }
+                else if(a[i]=='1' && b[j]=='0' && !carry){  // 100
+                    c += '1';
+                    carry = 0;
+                }
+                else if(a[i]=='0' && b[j]=='1' && carry){   // 011
+                    c += '0';
+                    carry = 1;
+                }
+                else if(a[i]=='0' && b[j]=='1' && !carry){  // 010
+                    c += '1';
+                    carry = 0;
+                }
+                else if(a[i]=='0' && b[j]=='0' && carry){   // 001
+                    c += '1';
+                    carry = 0;
+                }
+                else{                                       // 000
+                    c += '0';
+                    carry = 0;
+                }
+
+                i--;
+                j--;
+            }
+
+            else if(i >= 0 && j < 0){
+
+                if(carry && a[i]=='1'){
+                    c += '0';
+                    carry = 1;
+                }
+                else if(a[i]=='0' && carry){
+                    c += '1';
+                    carry = 0;
+                }
+                else{
+                    c += a[i];
+                }
+                i--;
+            }
+
+            else if(i<0 && j>=0){ // j >= 0
+             
+                if(carry && b[j]=='1'){
+                    c += '0';
+                    carry = 1;
+                }
+                else if(b[j]=='0' && carry){
+                    c += '1';
+                    carry = 0;
+                }
+                else{
+                    c += b[j];
+                }
+                j--;
+            }
+            else if(carry){
+                c+='1';
+                carry =0;
+            }
+        }
+
+        reverse(c.begin(), c.end());
+        return c;
     }
-
-    reverse(begin(ans), end(ans));
-    return ans;
-  }
 };

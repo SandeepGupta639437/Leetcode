406f3013d20a82618481edb43f14c7c8562690c5
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-02-21 17:42:56 +0530

    Time: 3 ms (72.17%), Space: 7.9 MB (88.15%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,19 +1,19 @@
 class Solution {
-    public:
-        bool prime(int s){
-                if(s==1)return false;
-                        if(s==2)return true;
-                                for(int i=2;i<=sqrt(s);i++){
-                                            if(s%i==0)return false;
-                                                    }
-                                                            return true;
-                                                                }
-                                                                    int countPrimeSetBits(int left, int right) {
-                                                                            int ans=0;
-                                                                                    for(int i=left;i<=right;i++){
-                                                                                               int setbit = __builtin_popcount(i);
-                                                                                                          if(prime(setbit))ans++;
-                                                                                                                  }
-                                                                                                                          return ans;
-                                                                                                                              }
-                                                                                                                              };
+public:
+    bool prime(int s){
+        if(s==1)return false;
+        if(s==2)return true;
+        for(int i=2;i<=sqrt(s);i++){
+            if(s%i==0)return false;
+        }
+        return true;
+    }
+    int countPrimeSetBits(int left, int right) {
+        int ans=0;
+        for(int i=left;i<=right;i++){
+            int setbit=__builtin_popcount(i);
+            if(prime(setbit))ans++;
+        }
+        return ans;
+    }
+};

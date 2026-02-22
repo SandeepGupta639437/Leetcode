c5d9303b557e0ac78ebb6dab91a167c1db300d69
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-02-22 11:17:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -4,19 +4,16 @@ public:
 
     vector<int> sortArray(vector<int>& nums) {
         for(int i = 0; i < 100001; ++i) count[i] = 0;
-        int min_v = 50000, max_v = -50000;
 
         for (int x : nums) {
-            count[x + 50000]++;
-            if (x < min_v) min_v = x;
-            if (x > max_v) max_v = x;
+            count[x]++;
         }
         
         int index=0;
-        for (int i=min_v+50000;i<=max_v+50000;++i) {
+        for (int i=0;i<100001;++i) {
             int c=count[i];
-            if (c>0){
-                int val = i - 50000;
+            if (c>=0){
+                int val = i;
                 while (c--) {
                     nums[index++] = val;
                 }

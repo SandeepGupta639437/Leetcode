1ee0f4eb78e76274f1cedc7a8c5b09b9373c432c
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-03-15 18:47:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -3,57 +3,29 @@ public:
     typedef long long ll;
 
     ll gcd(ll a,ll b){
-        if(b==0) return a;
+        if(b==0)return a;
         return gcd(b,a%b);
     }
-
     long long gcdSum(vector<int>& nums) {
-
         int n = nums.size();
 
-        vector<ll> mx(n);
-        mx[0] = nums[0];
-
-        for(int i=1;i<n;i++)
-            mx[i] = max(mx[i-1], (ll)nums[i]);
-
-        vector<ll> prefGCD(n);
-
-        for(int i=0;i<n;i++)
-            prefGCD[i] = gcd(mx[i], nums[i]);
-
-        map<ll,ll> freq;
-
-        for(ll x : prefGCD)
-            freq[x]++;
-
-        vector<ll> vals;
-        for(auto &p:freq)
-            vals.push_back(p.first);
-
-        ll l=0,r=vals.size()-1,ans=0;
-
-        while(l<=r){
-
-            ll a=vals[l], b=vals[r];
-
-            if(l==r){
-                ll cnt=freq[a]/2;
-                ans+=cnt*gcd(a,a);
-                break;
-            }
-
-            ll pairs=min(freq[a],freq[b]);
-
-            ans+=pairs*gcd(a,b);
-
-            freq[a]-=pairs;
-            freq[b]-=pairs;
-
-            if(freq[a]==0) l++;
-            if(freq[b]==0) r--;
+        vector<ll>mx(n);
+        vector<ll>prefGCD(n);
+        mx[0]=nums[0];
+        for(ll i=1;i<n;i++){
+            ll maxi = max(mx[i-1],1LL*nums[i]);
+            mx[i]=maxi;
         }
-
+        for(ll i=0;i<n;i++){
+            prefGCD[i]=gcd(mx[i],nums[i]);
+        }
+        sort(begin(prefGCD),end(prefGCD));
+        ll l=0,r=n-1,ans=0;
+        while(l<r){
+           ans+=gcd(prefGCD[l],prefGCD[r]);
+           l++;
+           r--;
+        }
         return ans;
     }
 };

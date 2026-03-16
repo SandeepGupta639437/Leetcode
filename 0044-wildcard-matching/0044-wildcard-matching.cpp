df673df1ab6c45c91a3a9df357f31ea2c35868d9
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-03-16 17:33:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-
-    bool solve(string &s, string &p, int i, int j){
-        if(i==s.size() && j==p.size()) return true;
-        if(j==p.size()) return false;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(i < s.size() && (s[i]==p[j] || p[j]=='?')){
-            return dp[i][j] = solve(s,p,i+1,j+1);
-        }
-
-        if(p[j]=='*'){
-            if(i < s.size() && solve(s,p,i+1,j)) return dp[i][j]=1;
-            if(solve(s,p,i,j+1)) return dp[i][j]=1;
-        }
-
-        return dp[i][j]=0;
-    }
-
     bool isMatch(string s, string p) {
-        dp.resize(s.size()+1, vector<int>(p.size()+1,-1));
-        return solve(s,p,0,0);
+        int i = 0, j = 0, starIdx = -1, match = 0;
+        while (i < s.size()) {
+            if (j < p.size() && (p[j] == '?' || p[j] == s[i])) {
+                i++; j++;
+            } else if (j < p.size() && p[j] == '*') {
+                starIdx = j;
+                match = i;
+                j++;
+            } else if (starIdx != -1) {
+                j = starIdx + 1;
+                match++;
+                i = match;
+            } else {
+                return false;
+            }
+        }
+        while (j < p.size() && p[j] == '*') j++;
+        return j == p.size();
     }
 };

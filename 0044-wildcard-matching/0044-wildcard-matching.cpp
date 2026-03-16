b28455f42b49eb6fd309bd98575cf1517e906e4c
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-03-16 17:53:12 +0530

    Time: 1422 ms (5.01%), Space: 26.9 MB (41.9%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
+    int dp[2001][2001];
 
     bool solve(string &s, string &p, int i, int j){
-        if(i==s.size() && j==p.size())return true;
+        if(i==s.size() && j==p.size())return dp[i][j]=true;;
         if(j==p.size()) return false;
+
+        if(dp[i][j]!=-1)return dp[i][j];
+
         if(i==s.size()){
-            if(p[j]=='*')return solve(s,p,i,j+1);
+            if(p[j]=='*')return dp[i][j]=solve(s,p,i,j+1);
             return false;
         }
-        if(p[j]=='?' || (s[i]==p[j]))return solve(s,p,i+1,j+1);
-        else if(p[j]=='*')return (solve(s,p,i+1,j)||solve(s,p,i,j+1));
+        if(p[j]=='?' || (s[i]==p[j]))return dp[i][j]= solve(s,p,i+1,j+1);
+        else if(p[j]=='*'){
+            return dp[i][j]= (solve(s,p,i+1,j)||solve(s,p,i,j+1));
+        }
         else return false;
 
     }
 
     bool isMatch(string s, string p) {
+        memset(dp,-1,sizeof(dp));
         return solve(s,p,0,0);
     }
 };

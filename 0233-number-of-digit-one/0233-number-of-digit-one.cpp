8e40818a9b0fff30b1a3eb38782e8447ba6d23b2
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-03-18 10:57:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0233-number-of-digit-one/0233-number-of-digit-one.cpp
+++ 0233-number-of-digit-one/0233-number-of-digit-one.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    int countDigitOne(int n) {
-        long long ans = 0;
-
-        for(long long div = 1; div <= n; div *= 10){
-            long long high = n / (div * 10);
-            long long cur  = (n / div) % 10;
-            long long low  = n % div;
-
-            if(cur == 0){
-                ans += high * div;
-            }
-            else if(cur == 1){
-                ans += high * div + (low + 1);
-            }
-            else{
-                ans += (high + 1) * div;
-            }
+    int cntOne(int n ){
+        int cnt =0;
+        while(n){
+            if(n%10==1)cnt++;
+            n/=10;
         }
-
-        return ans;
+        return cnt;
+    }
+    int countDigitOne(int n) {
+        int ans =0,prev=0;
+        map<int,int>mpp;
+        int j=99;
+        while( j<=n){
+            int ones = cntOne(j)*100 + 20;
+            mpp[j]=ones+prev;
+            // cout<<mpp[j]<<" "<<j<<endl;
+            prev=mpp[j];
+            j+=100;
+        }
+        int i=n;
+        // cout<<mpp[j-100]<<" "<<j-100<<" "<<n<<" "<<endl;
+        for(i=n;(i+1)%100!=0;i--){
+            ans+=cntOne(i);
+        }
+        // int temp=n%100;
+        // ans+=cntOne(n-temp);
+        // if(n%100==0 && n>99)ans++;
+        return mpp[j-100]+ans;
     }
 };

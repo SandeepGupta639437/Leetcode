e54e870a728773e5a6ce04cea4939cb4989ace1a
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-03-19 16:45:22 +0530

    Time: 70 ms (36.59%), Space: 130.4 MB (34.96%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -4,44 +4,35 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> sum(m, vector<int>(n, 0));
         vector<vector<int>> cntX(m, vector<int>(n, 0));
+        vector<vector<int>> cntY(m, vector<int>(n, 0));
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                
-                int val = 0;
-                if(grid[i][j] == 'X') val = 1;
-                else if(grid[i][j] == 'Y') val = -1;
-
-                sum[i][j] = val;
-                cntX[i][j] = (grid[i][j] == 'X');
-
-                if(i > 0){
-                    sum[i][j] += sum[i-1][j];
-                    cntX[i][j] += cntX[i-1][j];
-                }
-                if(j > 0){
-                    sum[i][j] += sum[i][j-1];
-                    cntX[i][j] += cntX[i][j-1];
-                }
-                if(i > 0 && j > 0){
-                    sum[i][j] -= sum[i-1][j-1];
-                    cntX[i][j] -= cntX[i-1][j-1];
-                }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+               if(grid[i][j]=='X')cntX[i][j]++;
+               else if(grid[i][j]=='Y')cntY[i][j]++;
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j = 1; j < n; j++){
+               cntX[i][j]+=cntX[i][j-1];
+               cntY[i][j]+=cntY[i][j-1];
+            }
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 1; j < m; j++){
+               cntX[j][i]+=cntX[j-1][i];
+               cntY[j][i]+=cntY[j-1][i];
             }
         }
 
         int ans = 0;
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(sum[i][j] == 0 && cntX[i][j] > 0){
-                    ans++;
-                }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(cntX[i][j]==cntY[i][j] && cntX[i][j]>0) ans++;
             }
         }
-
         return ans;
     }
 };

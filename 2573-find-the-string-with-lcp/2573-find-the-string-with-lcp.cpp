ab269f03f85348fe627903cd8092a0132d642fdf
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-03-28 23:20:49 +0530

    Time: 18 ms (34.94%), Space: 85 MB (25.3%) - LeetHub

---
+++ 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    vector<vector<int>>checkLCP(string &word){
+        int n = word.length();
+        vector<vector<int>>lcp(n,vector<int>(n,0));
+
+        for(int i=0;i<n;i++){
+            lcp[i][n-1] = (word[i] == word[n-1]) ? 1 : 0;
+        }
+        for(int i=0;i<n;i++){
+             lcp[n-1][i] = (word[n-1] == word[i]) ? 1 : 0;
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                if(word[i]==word[j]){
+                    lcp[i][j] = 1 + lcp[i+1][j+1];
+                }else{
+                    lcp[i][j] = 0;
+                }
+            }
+        }
+        return lcp;
+    }
+   
+    string findTheString(vector<vector<int>>& lcp) {
+        int n = lcp.size();
+        string word(n,'$');
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(lcp[j][i]!=0){
+                    word[i] = word[j];
+                    break;
+                }
+            }
+            if(word[i]=='$'){
+                vector<bool>forbidden(26,false);
+                for(int j=0;j<i;j++){
+                    if(lcp[j][i]==0){
+                        forbidden[word[j]-'a']=true;
+                    }
+                }
+
+                for(int j=0;j<26;j++){
+                    if(forbidden[j]==false){
+                        word[i] = j+'a';
+                        break;
+                    }
+                }
+                if(word[i]=='$'){
+                    return "";
+                }
+            }
+        }
+
+        return checkLCP(word)==lcp ? word : "";
+    }
+};

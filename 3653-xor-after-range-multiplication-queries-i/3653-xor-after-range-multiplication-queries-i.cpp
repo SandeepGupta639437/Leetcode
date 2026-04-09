884351061287b0e3eb825acd59d70be31c2ed338
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-04-09 07:24:15 +0530

    Time: 162 ms (5.29%), Space: 169.2 MB (23.56%) - LeetHub

--- 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.cpp
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.cpp
@@ -3,9 +3,6 @@ public:
     using ll = long long;
     const ll MOD = 1e9+7;
     int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
-        int m = queries.size();
-
         for(auto it:queries){
             int idx = it[0];
             while(idx<=it[1]){
@@ -18,6 +15,5 @@ public:
             ans^=it;
         }
         return ans;
-        
     }
 };

0e2de2e5c767775f7aa8ca145f633a52b690a692
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-04-19 00:52:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    using ll = long long;
-    int dp[10010][301];
-    ll solve(int amount , vector<int>& coins,int j){
-        if(amount == 0)return 1;
-        ll ways = 0;
-        if(dp[amount][j]!=-1)return dp[amount][j];
-        for(int i=j;i<coins.size();i++){
-         if((amount-coins[i])>=0) ways += solve((amount-coins[i]),coins,i);
-        }
-        return dp[amount][j]=ways;
+    int dp[5001][301];
+
+    int solve(int amount, vector<int>& coins, int j) {
+        if (amount == 0) return 1;
+        if (j >= coins.size()) return 0;
+
+        if (dp[amount][j] != -1) return dp[amount][j];
+
+        int ways = 0;
+
+        // take
+        if (amount - coins[j] >= 0)
+            ways += solve(amount - coins[j], coins, j);
+
+        // skip
+        ways += solve(amount, coins, j + 1);
+
+        return dp[amount][j] = ways;
     }
+
     int change(int amount, vector<int>& coins) {
-        memset(dp,-1,sizeof(dp));
-        sort(begin(coins),end(coins));
-        int ans = solve(amount,coins,0);
-        return ans;
+        memset(dp, -1, sizeof(dp));
+        return solve(amount, coins, 0);
     }
 };

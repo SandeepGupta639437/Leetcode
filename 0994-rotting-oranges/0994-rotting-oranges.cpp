29d2446f1d7343d0aba9ba6bb8fdfb14a184d75e
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-04-19 14:13:12 +0530

    Time: 0 ms (100%), Space: 17.1 MB (29.11%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,56 +1,57 @@
 class Solution {
 public:
-    typedef pair<int,int>P;
+    typedef pair<int,int> P;
 
-    vector<vector<int>> directions{{-1,0},{1,0},{0,-1},{0,1}};
-
-    bool isSafe(int i, int j,int n,int m){
-        if(i>=m || i<0 || j<0 || j>=n)return false;
-        return true;
+    bool isSafe(int i,int j,int n,int m,vector<vector<int>>& grid){
+        if(i>=0 && j>=0 && i<m && j<n && grid[i][j]==1)return true;
+        return false;
     }
 
+    vector<vector<int>>direction{{-1,0},{1,0},{0,-1},{0,1}};
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        queue<P>que;
-        int fresh = 0;
+        queue<P>que;  // it will store the quards of cell
+        int freshCount  = 0;
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j ++){
-               if(grid[i][j]==2){
-                que.push({i,j});
-               }
-               else if(grid[i][j]==1)fresh++;
-            }
-        }
-        if(!fresh)return 0;
+        for(int i=0;i<m;i++){
 
-        int minutes=0;
-
-        while(!que.empty()){
-            int size = que.size();
-            while(size--){
-                P curr = que.front();
-                que.pop();
-
-                int i = curr.first;
-                int j = curr.second;
-
-                for(auto dir : directions){
-                    int new_i = i+dir[0];
-                    int new_j = j+dir[1];
-
-                    if(isSafe(new_i,new_j,n,m) && grid[new_i][new_j]==1){
-                        grid[new_i][new_j]=2;
-                        que.push({new_i,new_j});
-                        fresh--;
-                    }
+            for(int j = 0; j<n ;j++){
+                if(grid[i][j] == 2){
+                    que.push({i,j});
+                }else if(grid[i][j]==1){
+                    freshCount++;
                 }
             }
-            minutes++;
         }
-        if(!fresh)return minutes-1;
-        return -1;
+
+        if(freshCount == 0)return 0; //all oranges are rotten;
+
+        int minutes = 0;
+
+        while(!que.empty()){
+           int siz = que.size();
+           while(siz--){
+              P curr = que.front();
+              que.pop();
+
+              int i = curr.first;
+              int j = curr.second;
+
+              for(vector<int>&dir : direction){
+                int new_i = i+dir[0];
+                int new_j = j+dir[1];
+
+                if(isSafe(new_i,new_j,n,m,grid)){
+                    grid[new_i][new_j] = 2;
+                    que.push({new_i,new_j});
+                    freshCount--;
+                }
+              }
+           }
+           minutes++;
+        }
+      return freshCount == 0 ? (minutes-1) : -1;
     }
 };

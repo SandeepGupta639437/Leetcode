6f4812b60cb4587cec002e68d28a36b73550d5eb
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-04-28 18:55:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -3,25 +3,28 @@ public:
     int minOperations(vector<vector<int>>& grid, int x) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<int>a;
-        int rem = grid[0][0]%x;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+
+        vector<int> a;
+        int rem = grid[0][0] % x;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] % x != rem) return -1;
                 a.push_back(grid[i][j]);
-                if(grid[i][j]%x!=rem)return -1;
             }
         }
 
-        sort(a.begin(),a.end());
-        int mid = (n*m)/2;
-        int k = a[mid];
+        int mid = (n * m) / 2;
+
+        nth_element(a.begin(), a.begin() + mid, a.end()); // FIX
+
+        int median = a[mid];
         int ans = 0;
 
-        for(int i=0;i<n*m;i++){
-            ans+=(abs(k-a[i]))/x;
+        for (int val : a) {
+            ans += abs(val - median) / x;
         }
 
         return ans;
-        
     }
 };

16a3a0886a402ca212dab31218cc387b0837245d
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-05-03 19:00:32 +0530

    Time: 1 ms (44.09%), Space: 8.2 MB (38.8%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.cpp
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
-    bool check(int n){
-        bool changed = false;
-        while(n>0){
-            int d = n%10;
-            n/=10;
-            if(d==3 ||d==4 || d==7)return false;
-            if(d==2 || d==5 || d==9 || d==6)changed = true;
-        }
-        return changed;
+    int dp[100001];
+    int solve(int n){
+        if(dp[n]!=-1)return dp[n];
+        if(n==0)return dp[n] =0;
+
+        int remain = solve(n/10);
+        if(remain == 2) return dp[n] = 2;
+        int digit_check;
+        int d = n%10;
+
+        if(d==1 || d == 0 || d == 8)digit_check = 0;
+        else if(d==2 || d==5 || d==6 || d==9) digit_check = 1;
+        else return dp[n] = 2;
+
+        if(digit_check == 0 && remain == 0)return dp[n] = 0;
+
+        return dp[n]=1;
     }
     int rotatedDigits(int n) {
         int ans = 0;
+        memset(dp,-1,sizeof(dp));
         for(int i = 1;i<=n;i++){
-            if(check(i))ans++;
+            if(solve(i)==1)ans++;
         }
         return  ans;
     }

4556b57e59f3a7f073b057f0ae5b270282d62f44
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-05-09 23:58:27 +0530

    Time: 0 ms (100%), Space: 53.6 MB (78.75%) - LeetHub

---
+++ 3921-score-validator/3921-score-validator.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> scoreValidator(vector<string>& events) {
+        int n = events.size();
+        int score = 0, counter = 0;
+        for(int i = 0;i<n;i++){
+            if(events[i]=="W"){
+                counter++;
+            }else if(events[i]=="WD" || events[i]=="NB"){
+                score++;
+            }else{
+                score+=stoi(events[i]);
+            }
+            if(counter == 10 )break;
+        }
+        return {score,counter};
+    }
+};

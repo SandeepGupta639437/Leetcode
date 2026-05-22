41570d6e8ce91dec646cde5dd7151a4288cea6ed
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-05-23 02:49:08 +0530

    Time: 59 ms (15.5%), Space: 26.4 MB (13.79%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -7,11 +7,11 @@ public:
         for(int i=0;i < 10 && i<s.size();i++)
         {
             p+=s[i];
-        }
+        } // make first window of size 10
         m[p]=1;
         for(int i=10;i<s.size();i++)
         {
-            p=p.substr(1)+s[i];
+            p=p.substr(1)+s[i]; // sliding the window
             if(m.count(p) != 0 && m[p] == 1)
             {
                 res.push_back(p);

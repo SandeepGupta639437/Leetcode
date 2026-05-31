585fd7a1aaa71440ed3799e04137ce9df1f43b87
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-05-31 05:38:25 +0530

    Time: 1768 ms (5.79%), Space: 485.6 MB (6.94%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
-
-    bool isPossible(vector<vector<int>>& tasks, int mid) {
-        for(auto &task : tasks) {
-            int actual  = task[0];
-            int minimum = task[1];
-
-            if(minimum > mid) {
-                return false;
-            }
-
-            mid -= actual;
+    bool possible(int mid,vector<vector<int>>& tasks){
+        int energy = mid;
+        for(auto it : tasks){
+            if(it[1]>energy)return false;
+            else energy-=it[0];
         }
-
         return true;
     }
-
     int minimumEffort(vector<vector<int>>& tasks) {
-        int n = tasks.size();
-
-        int l = 0;
-        int r = 1e9;
-
-        int result = INT_MAX;
-
-        auto lambda = [](auto &task1, auto& task2) {
+        int start = 0, last = 1e9;
+        int ans = 1e9;
+       auto lambda = [](auto &task1, auto& task2) {
             int diff1 = task1[1] - task1[0];
             int diff2 = task2[1] - task2[0];
 
@@ -33,18 +20,15 @@ public:
 
         sort(begin(tasks), end(tasks), lambda);
 
-
-        while(l <= r) {
-            int mid = l + (r-l)/2;
-
-            if(isPossible(tasks, mid)) {
-                result = mid;
-                r = mid-1;
-            } else {
-                l = mid+1;
+        while(start<=last){
+            int mid = start - (start-last)/2;
+            if(possible(mid,tasks)){
+                ans = min(ans,mid);
+                last = mid-1;
+            }else{
+                start = mid + 1;
             }
         }
-
-        return result;
+        return ans;
     }
 };

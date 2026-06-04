98354f4bd57f7fb8bd2d5ac1c1719d7f3f5b490e
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-06-04 08:33:16 +0530

    Time: 12 ms (86.57%), Space: 9.1 MB (88.62%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
+    int dp[100005];
     int waviness(int n) {
+        int p = n;
         if (n < 100) return 0;
         int cnt = 0;
+        if(dp[n]!=-1)return dp[n];
         int r = n % 10;n /= 10;
         int m = n % 10;n /= 10;
         while (n) {
             int l = n % 10;
             if ((m > l && m > r) || (m < l && m < r))cnt++;
+            if(dp[n]!=-1){
+                r = m;
+                m = l;
+                l = (n/10)%10;
+                if ((m > l && m > r) || (m < l && m < r))cnt++;
+                return cnt+dp[n];
+            }
             r = m;
             m = l;
             n /= 10;
         }
-        return cnt;
+        return dp[p] = cnt;
     }
     int totalWaviness(int num1, int num2) {
         int ans = 0;
+        memset(dp,-1,sizeof(dp));
         for(int i=num1;i<=num2;i++){
             ans+=waviness(i);
         }

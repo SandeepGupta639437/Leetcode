926313287ffa0c48dbf629ee3a0b2a30964518d2
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-06-16 15:12:01 +0530

    Time: 3 ms (76.89%), Space: 39.8 MB (42.05%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result;
+        for(auto ch:s){
+            if(ch=='*'){
+                if(result.size()!=0)result.pop_back();
+            }
+            else if(ch=='#')result+=result;
+            else if(ch=='%')reverse(result.begin(),result.end());
+            else result+=ch;
+        }
+        return result;
+    }
+};

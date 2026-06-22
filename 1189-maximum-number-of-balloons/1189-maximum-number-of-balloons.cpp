87f2dc3b7c78231f9ed9ba3d23fed3f7d7b5d6c8
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-06-22 13:10:29 +0530

    Time: 0 ms (100%), Space: 8.8 MB (95.38%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -10,12 +10,8 @@ public:
             if(text[i]=='o')counto++;
             if(text[i]=='n')countn++;
         }
-        int b=countb;
-        int a=counta;
-        int l=countl/2;
-        int o=counto/2;
-        int n=countn;
-        int ans1=min({b,a,l,o,n});
-        return ans1;
+        int b=countb,a=counta,l=countl/2,o=counto/2,n=countn;
+        
+        return min({b,a,l,o,n});
     }
 };

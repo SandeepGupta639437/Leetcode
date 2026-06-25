7200ae3492f2be5d83e3e0aedd281683b6aea696
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-06-25 10:27:10 +0530

    Time: 4 ms (51.79%), Space: 15.4 MB (8.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -9,11 +9,11 @@ public:
         int n = nums.size();
         for (int i = 0; i < n; i++) {
             int complement = target - nums[i];
-            if (mpp.find(complement) != mpp.end()) {
-                return {mpp[complement], i}; // Return indices of the two numbers
+            if (mpp[complement] != 0) {
+                return {mpp[complement]-1, i}; 
             }
-            mpp[nums[i]] = i; // Store the index of the current number
+            mpp[nums[i]] = i+1;
         }
-        return {}; // Return an empty vector if no solution is found
+        return {};
     }
 };

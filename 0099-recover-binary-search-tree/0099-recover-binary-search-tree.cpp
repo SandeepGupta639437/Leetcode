271c65c23691197fcb84d5d95b2c27bbcdcc1e4e
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-06-27 09:35:54 +0530

    Time: 0 ms (100%), Space: 60.8 MB (78.06%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -27,14 +27,33 @@ public:
         fix(root->right, vals, idx);
     }
 
+    TreeNode* first;
+    TreeNode* middle;
+    TreeNode* last;
+    TreeNode* prev;
+    void inorder(TreeNode* root){
+        if(!root) return;
+
+        inorder(root->left);
+        if(prev!=NULL && (root->val < prev->val)){
+            if(first==NULL){
+                first = prev;
+                middle = root;
+            }else{
+                last = root;
+            }
+        }
+
+        prev = root;
+        inorder(root->right);
+    }
+
     void recoverTree(TreeNode* root) {
-        vector<int> vals;
+        first = middle = last = NULL;
+        prev = new TreeNode(INT_MIN);
+        inorder(root);
 
-        inorder(root, vals);
-
-        sort(vals.begin(), vals.end());
-
-        int idx = 0;
-        fix(root, vals, idx);
+        if(first && last) swap(first->val,last->val);
+        else if(first && middle) swap(first->val,middle->val);
     }
 };

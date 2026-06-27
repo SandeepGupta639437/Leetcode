29d333955d1be8606009e80482c753a31af25831
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-06-27 22:41:51 +0530

    Time: 13 ms (16.15%), Space: 17 MB (80.31%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -3,15 +3,29 @@ public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<int>new1;
+        // vector<int>new1;
+        // for(int i=0;i<n;i++){
+        //  for(int j=0;j<m;j++){
+        //     new1.push_back(matrix[i][j]);
+        //  }
+        // }
+        // // sort(new1.begin(),new1.end());
+        // nth_element(new1.begin(), new1.begin() + k - 1, new1.end());
+        // int ans=new1[k-1];
+        // return ans;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int, pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
         for(int i=0;i<n;i++){
-         for(int j=0;j<m;j++){
-            new1.push_back(matrix[i][j]);
-         }
+            pq.push({matrix[i][0], {i, 0}});
         }
-        // sort(new1.begin(),new1.end());
-        nth_element(new1.begin(), new1.begin() + k - 1, new1.end());
-        int ans=new1[k-1];
-        return ans;
+        while(k--){
+            auto [val, pos] = pq.top();
+            auto [row, col] = pos;
+            pq.pop();
+            if (col+1<m) {
+                pq.push({matrix[row][col + 1], {row, col + 1}});
+            }
+            if(k==0)return val;
+        }
+        return pq.top().first;
     }
 };

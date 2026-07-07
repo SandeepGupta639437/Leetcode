7fe21379414967a60b3e996901a36682753c8400
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-07-08 01:42:04 +0530

    Time: 4 ms (12.64%), Space: 29.9 MB (88.64%) - LeetHub

--- 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.cpp
+++ 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -1,32 +1,15 @@
 class Solution {
+private:
+    void rec(int i, int prev, vector<string>& words, vector<int>& groups, vector<string>& ans) {
+        if (i == words.size()) return;
+        if (prev != groups[i]) ans.push_back(words[i]);
+        rec(i + 1, groups[i], words, groups, ans);
+    }
+
 public:
     vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
-        vector<string>ans1,ans2;
-        int flag = 0;
-
-        int n = words.size();
-        for(int i=0;i<n;i++){
-            if(flag == 0 && groups[i]==0){
-                ans1.push_back(words[i]);
-                flag = !flag;
-            }
-            if(flag && groups[i] == 1) {
-                ans1.push_back(words[i]);
-                flag = !flag;
-            }
-        }
-        flag = 0;
-        for(int i=0;i<n;i++){
-            if(flag == 1 && groups[i]==0){
-                ans2.push_back(words[i]);
-                flag = !flag;
-            }
-            if(flag == 0 && groups[i] == 1) {
-                ans2.push_back(words[i]);
-                flag = !flag;
-            }
-        }
-        if(ans1.size()>ans2.size())return ans1;
-        return ans2;
+        vector<string> ans;
+        rec(0, -1, words, groups, ans);
+        return ans;
     }
 };

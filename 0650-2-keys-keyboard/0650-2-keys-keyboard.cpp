e188506ba95f5909e9bebfaa23822fcea610d727
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-07-08 01:29:55 +0530

    Time: 15 ms (31.02%), Space: 12.2 MB (25.49%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,21 +1,24 @@
 class Solution {
 private:
     int targetLength;
+    int dp[1001][1001];
 
     int findMinSteps(int currentLength, int clipboardLength) {
         if (currentLength == targetLength) return 0;
         if (currentLength > targetLength) return INT_MAX / 2;
+        if(dp[currentLength][clipboardLength]!=-1)return dp[currentLength][clipboardLength];
 
         int copyAndPaste = 2 + findMinSteps(currentLength * 2, currentLength);
         int pasteOnly = 1 + findMinSteps(currentLength + clipboardLength, clipboardLength);
 
-        return min(copyAndPaste, pasteOnly);
+        return dp[currentLength][clipboardLength] = min(copyAndPaste, pasteOnly);
     }
 
 public:
     int minSteps(int n) {
         if (n == 1) return 0;
         targetLength = n;
+        memset(dp,-1,sizeof(dp));
         return 1 + findMinSteps(1, 1);
     }
 };

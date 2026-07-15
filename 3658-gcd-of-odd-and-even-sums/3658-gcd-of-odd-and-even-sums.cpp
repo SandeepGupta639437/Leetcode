a806387c986f36b6cf1d32157118eb7f7fc44872
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-07-15 14:23:53 +0530

    Time: 3 ms (16.66%), Space: 8.7 MB (9.11%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
-    long long gcd(long long a, long long b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
-    }
     int gcdOfOddEvenSums(int n) {
-        long long sumEven=n*(n+1);
-        long long sumOdd=n*n;  
-        return gcd(sumEven,sumOdd);
+        return n;
     }
 };

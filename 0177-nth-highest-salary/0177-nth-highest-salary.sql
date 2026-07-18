b70fb7e29ec15531fc1612f0fefc9086afe429cc
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-07-18 19:04:51 +0530

    Time: 425 ms (91.11%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -4,7 +4,7 @@ SET N = N-1;
   RETURN (
       # Write your MySQL query statement below.
       SELECT DISTINCT salary 
-      FROM Employee ORDER BY salary DESC LIMIT 1 OFFSET N
+      FROM Employee ORDER BY salary DESC LIMIT N,1
 
   );
 END

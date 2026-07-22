6af723b861a5961363cbb8dd71506372dc2305d2
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-07-22 12:11:06 +0530

    Time: 676 ms (9.41%), Space: 0B (100%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,9 +1,4 @@
 SELECT
-    player_id ,
-    event_date AS first_login 
-FROM Activity a1
-WHERE event_date = (
-    SELECT MIN(event_date)
-    FROM Activity a2
-    WHERE a1.player_id = a2.player_id
-)
+    DISTINCT player_id ,
+    MIN(event_date) OVER(PARTITION by player_id) AS first_login 
+FROM Activity

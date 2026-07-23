fa8bc4f72f9a0f68ca0e992fb2834bc2bfd322d0
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-07-23 11:34:22 +0530

    Time: 0 ms (100%), Space: 198.8 MB (85%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -3,12 +3,14 @@ public:
     int uniqueXorTriplets(vector<int>& nums) {
         int n = nums.size();
 
-        if (n == 1) return 1;
-        if (n == 2) return 2;
+        // edge case
+        if (n == 1) return 1; // zero not possible
+        if (n == 2) return 2; // zero not posible (like  {1,2,3});
 
+        // let the max number is 7 .. and 7 is 3 bit number .. so we can generfate all 3 bit number including 0 till y7 ..i.e: cnt = 8;
         int p = 1;
         while (p <= n)
-            p <<= 1;
+            p *= 2;     // It gives the number of
 
         return p;
     }

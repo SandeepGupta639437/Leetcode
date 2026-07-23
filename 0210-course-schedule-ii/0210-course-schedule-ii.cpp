b136bf03f9095c1d6fa866a7a6df06e3cbff945e
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-07-23 19:01:42 +0530

    Time: 0 ms (100%), Space: 18.3 MB (56.71%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& adj, int u, vector<int>& vis, stack<int>& st) {
-        vis[u] = 1; // visiting
-
-        for (int v : adj[u]) {
-            if (vis[v] == 1)
-                return false; // cycle
-
-            if (vis[v] == 0) {
-                if (!dfs(adj, v, vis, st))
-                    return false;
-            }
-        }
-
-        vis[u] = 2; // visited
-        st.push(u);
-        return true;
-    }
-
     vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(n);
+        vector<int> indegree(n, 0);
 
+        // Build graph and indegree
         for (auto &it : prerequisites) {
-            adj[it[1]].push_back(it[0]);
+            int u = it[1];
+            int v = it[0];
+            adj[u].push_back(v);
+            indegree[v]++;
         }
 
-        vector<int> vis(n, 0);
-        stack<int> st;
+        queue<int> q;
 
+        // Push all nodes with indegree 0
         for (int i = 0; i < n; i++) {
-            if (vis[i] == 0) {
-                if (!dfs(adj, i, vis, st))
-                    return {};
-            }
+            if (indegree[i] == 0)
+                q.push(i);
         }
 
         vector<int> ans;
 
-        while (!st.empty()) {
-            ans.push_back(st.top());
-            st.pop();
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+
+            ans.push_back(u);
+
+            for (int v : adj[u]) {
+                indegree[v]--;
+
+                if (indegree[v] == 0)
+                    q.push(v);
+            }
         }
 
-        return ans;
+        // If all courses are processed, return the order
+        if (ans.size() == n)
+            return ans;
+
+        // Cycle exists
+        return {};
     }
 };

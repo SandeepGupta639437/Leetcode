c737d2cae441ad49135c634e3f0ba22451e09000
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-07-23 19:25:20 +0530

    Time: 5 ms (42.94%), Space: 18.9 MB (34.08%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,46 +1,35 @@
 class Solution {
 public:
     vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(n);
+        vector<vector<int>> adj(n); 
         vector<int> indegree(n, 0);
 
-        // Build graph and indegree
-        for (auto &it : prerequisites) {
-            int u = it[1];
-            int v = it[0];
-            adj[u].push_back(v);
-            indegree[v]++;
+        for(auto it: prerequisites){
+            int course = it[0];
+            int preReq = it[1];
+            adj[preReq].push_back(course);
+            indegree[course]++;
         }
 
-        queue<int> q;
-
-        // Push all nodes with indegree 0
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0)
-                q.push(i);
+        queue<int>q;
+        for(int i = 0;i<n;i++){
+            if(indegree[i]==0)q.push(i);
         }
 
         vector<int> ans;
 
-        while (!q.empty()) {
+        while(!q.empty()){
             int u = q.front();
             q.pop();
-
             ans.push_back(u);
 
-            for (int v : adj[u]) {
-                indegree[v]--;
-
-                if (indegree[v] == 0)
-                    q.push(v);
+            for(auto it:adj[u]){
+                indegree[it]--;
+                if(indegree[it]==0)q.push(it);
             }
         }
 
-        // If all courses are processed, return the order
-        if (ans.size() == n)
-            return ans;
-
-        // Cycle exists
+        if(ans.size()==n)return ans;
         return {};
     }
 };

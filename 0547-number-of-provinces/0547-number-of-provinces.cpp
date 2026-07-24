7fe41e75d5effaf77dc757fcdae9003441b22f95
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-07-24 13:01:32 +0530

    Time: 0 ms (100%), Space: 19.3 MB (92.9%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,70 +1,52 @@
-class Solution {
-    // int n;
-    // void dfs(unordered_map<int,vector<int>>&adj,int u, vector<bool> &visited ){
-    //     visited[u]=true;
-    //     for(int &v : adj[u]){
-    //         if(!visited[v]){
-    //             dfs(adj,v,visited);
-    //         }
-    //     }
-    // }
-public:
-    int parent[205],size[205];
-    void make(int v){
-        parent[v] =v;
-        size[v] =1;
-    }
+class DSU{
+    public:
+       vector<int>parent;
+       vector<int>size;
+       DSU(int n){
+         parent.resize(n);
+         size.resize(n);
+       }
 
-    int find(int v){
-        if(parent[v]==v)return v;
-        return parent[v] = find(parent[v]);
-    }
+       void make(int v){
+         parent[v] = v;
+         size[v] = 1;
+       }
 
-    void Union(int a,int b){
-        a = find(a);
-        b = find(b);
-        if(a!=b){
+       int find(int v){
+         if(parent[v] == v)return v;
+         return parent[v] = find(parent[v]);
+       }
+
+       void Union(int a,int b){
+          a = find(a);
+          b = find(b);
+          if(a!=b){
             if(size[a]<size[b])swap(a,b);
             parent[b] = a;
-            size[a]+=size[b];
-        }
-    }
+            size[a] += size[b];
+          }
+       }
+};
 
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        // n=isConnected.size();
-        // vector<bool>visited(n,false);
-        // unordered_map<int,vector<int>>adj;
-
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-        //         if(isConnected[i][j]==1){
-        //             adj[i].push_back(j);
-        //             adj[j].push_back(i);
-        //         }
-        //     }
-        // }
-        // int cnt=0;
-        // for(int i=0;i<n;i++){
-        //     if(!visited[i]){
-        //         dfs(adj,i,visited);
-        //         cnt++;
-        //     }
-        // }
         int m = isConnected.size();
         int n = isConnected[0].size();
+        DSU c(n + 1);
         for(int i = 1; i <= m; i++){
-            make(i);
+            c.make(i);
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(isConnected[i][j]==1){
-                    Union(i+1,j+1);
+                    c.Union(i+1,j+1);
                 }
             }
         } 
         int cnt =0;
         for(int i=1;i<=m;i++){
-            if(parent[i]==i)cnt++;
+            if(c.parent[i]==i)cnt++;
         }
       return cnt;
     }

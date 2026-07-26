e123760e8fb4be28d213136f9a2c6bda761c6de4
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-07-26 11:08:22 +0530

    Time: 56 ms (63.32%), Space: 122.8 MB (47%) - LeetHub

--- 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
+++ 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
@@ -1,34 +1,32 @@
 class TreeAncestor {
-public:
-    vector<vector<int>> ancestorTable;
-    int rows;
-    int cols;
+    public:
+        vector<vector<int>> up;
+        int rows , cols;
 
-    TreeAncestor(int n, vector<int>& parent) {
-        rows = n;
-        cols = log2(n)+1;
-        ancestorTable.resize(rows,vector<int>(cols,-1));
+        TreeAncestor(int  n, vector<int>& parent){
+          rows = n;
+          cols = log2(n)+1;
+          up.resize(rows,vector<int>(cols,-1));
+          for(int i=0;i<n;i++){
+            up[i][0] = parent[i];
+          }
 
-        for(int node = 0;node<n;node++){
-            ancestorTable[node][0] = parent[node];
-        }
-
-        for(int j=1;j<cols;j++){
-            for(int node = 0;node<rows;node++){
-                if(ancestorTable[node][j-1]!=-1)ancestorTable[node][j] = ancestorTable[ancestorTable[node][j-1]][j-1];
+          for(int j=1;j<cols;j++){
+            for(int i =0;i<rows;i++){
+               if(up[i][j-1]!=-1)up[i][j] = up[up[i][j-1]][j-1];
             }
+          }
         }
-    }
-    
-    int getKthAncestor(int node, int k) {
-        for(int j=0;j<cols;j++){
-            if(k & (1<<j)){
-                node = ancestorTable[node][j];
-                if(node == -1)return -1;
+
+        int getKthAncestor(int node,int k){
+          for(int j=0;j<cols;j++){
+            if(k&(1<<j)){
+              node = up[node][j];
+              if(node == -1) return -1;
             }
+          }
+          return node;
         }
-        return node;
-    }
 };
 
 /**

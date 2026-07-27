9291e38809fab9fb228b7da91bcf6a5f4c44f49b
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-07-28 01:01:37 +0530

    Time: 62 ms (43.68%), Space: 123.3 MB (40.49%) - LeetHub

--- 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
+++ 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
@@ -1,36 +1,34 @@
-class TreeAncestor {
-    public:
-        vector<vector<int>> up;
-        int rows , cols;
+class TreeAncestor{
+public:
+  vector<vector<int>>Ancestor;
+  int rows , cols;
 
-        TreeAncestor(int  n, vector<int>& parent){
-          rows = n;
-          cols = log2(n)+1;
-          up.resize(rows,vector<int>(cols,-1));
-          for(int i=0;i<n;i++){
-            up[i][0] = parent[i];
-          }
+  TreeAncestor(int n,vector<int>parent){
+    rows = n;
+    cols = log2(n) + 1;
+    Ancestor.resize(rows,vector<int>(cols,-1));
 
-          for(int j=1;j<cols;j++){
-            for(int i =0;i<rows;i++){
-               if(up[i][j-1]!=-1)up[i][j] = up[up[i][j-1]][j-1];
-            }
-          }
+    for(int i=0;i<n;i++){
+      Ancestor[i][0] = parent[i];
+    }
+
+    for (int j = 1; j < cols; j++) {
+        for (int node = 0; node < rows; node++) {
+            if (Ancestor[node][j - 1] != -1)
+                Ancestor[node][j] = Ancestor[Ancestor[node][j - 1]][j - 1];
         }
+    }
+  }
 
-        int getKthAncestor(int node,int k){
-          for(int j=0;j<cols;j++){
-            if(k&(1<<j)){
-              node = up[node][j];
-              if(node == -1) return -1;
-            }
-          }
-          return node;
-        }
+  int getKthAncestor(int node,int k){
+    for(int j=0;j<cols;j++){
+      if(k& (1<<j)){
+        node =  Ancestor[node][j];
+        if(node == -1)return -1;
+      }
+    }
+    return node;
+  }
+
 };
 
-/**
- * Your TreeAncestor object will be instantiated and called as such:
- * TreeAncestor* obj = new TreeAncestor(n, parent);
- * int param_1 = obj->getKthAncestor(node,k);
- */

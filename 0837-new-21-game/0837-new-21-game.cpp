832ed407171c77ec9ec8a54e4990d5f36cca2a21
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-07-27 22:45:38 +0530

    Time: 3 ms (64.03%), Space: 13 MB (19.68%) - LeetHub

---
+++ 0837-new-21-game/0837-new-21-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        vector<double> dp(n+1,0);
+        dp[0] = 1;
+
+        double currProbabSum =  (k==0) ? 0 : 1;
+
+        for(int i=1;i<=n;i++){
+            dp[i] = currProbabSum / maxPts;
+            if(i<k) currProbabSum += dp[i];
+            if(i-maxPts>=0 && i-maxPts<k){
+                currProbabSum -= dp[i-maxPts];
+            }
+        }
+        return accumulate(begin(dp)+k,end(dp),0.0);
+    }
+};

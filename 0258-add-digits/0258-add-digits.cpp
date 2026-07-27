080dec634cb4995fb4dc27f2b3954f189b7da8f0
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-07-27 20:55:39 +0530

    Time: 0 ms (100%), Space: 8.5 MB (98.98%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
-    int solve(int num){
-        int temp = 0;
-        while(num){
-            temp = temp + num%10;
-            num/=10;
-        }
-        return temp;
-    }
     int addDigits(int num) {
-        while(num>9){
-            num = solve(num);
-        }
-        return num;
+       if(num<=9)return num;
+       if(num%9)return num%9;
+       return 9;
     }
 };

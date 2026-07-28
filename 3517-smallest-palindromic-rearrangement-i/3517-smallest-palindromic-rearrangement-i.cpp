e82010c0d05f58a1de82f7ea9c0bb6277316cd25
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-07-28 10:41:28 +0530

    Time: 103 ms (26.81%), Space: 70.2 MB (58.9%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -9,7 +9,7 @@ public:
 
         string ans = left;
 
-        if(n&1) ans += s[n / 2];
+        if(n&1) ans += s[n/2];   //(if 1 indexed  the ans+=s[n/2+1])
 
         reverse(left.begin(), left.end());
         ans += left;

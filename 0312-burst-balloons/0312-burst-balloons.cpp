ecc4c6da5a26714bdf581dc7ef7062318e5e8397
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-07-28 14:37:32 +0530

    Time: 151 ms (55.17%), Space: 11 MB (98.59%) - LeetHub

---
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int dp[301][301];
+    int solve(int l, int r, vector<int>& nums) {
+
+        if (l > r) return 0;
+
+        int ans = 0;
+        if(dp[l][r]!=-1)return dp[l][r];
+
+        for (int k = l; k <= r; k++) {
+            int coins = nums[l - 1] * nums[k] * nums[r + 1] + solve(l, k - 1, nums) + solve(k + 1, r, nums);
+
+            ans = max(ans, coins);
+        }
+
+        return dp[l][r] = ans;
+    }
+
+    int maxCoins(vector<int>& nums) {
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        memset(dp,-1,sizeof(dp));
+
+        return solve(1, nums.size() - 2, nums);
+    }
+};

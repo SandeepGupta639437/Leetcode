284700fd12ad82ce4175940c143652f1fce28dee
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-08-01 16:44:30 +0530

    Time: 76 ms (57.29%), Space: 93.2 MB (52.59%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -2,27 +2,20 @@ class Solution {
 public:
     int n;
     int dp[500001];
-    int getNextIdx(int i,vector<vector<int>>& v){
-        int val = v[i][1];
-        int l = i+1, r =  n-1;
-        int ans = -2;
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            if(v[mid][0]>=val){
-                r =  mid-1;
-                ans = mid;
-            }else{
-                l = mid+1;
-            }
-        }
-        return ans;
-    }
     int solve(int i,vector<vector<int>>& v){
-        if(i>=n || i<0)return 0;
+        if(i>=n)return 0;
 
         if(dp[i]!=-1)return dp[i];
 
-        int idx = getNextIdx(i,v);
+         auto it = lower_bound( v.begin() + i + 1, v.end(),
+            v[i][1],                // current end time
+            [&](const vector<int>& job, int endTime) {
+                return job[0] < endTime;
+            }
+        );
+
+        int idx = it - v.begin();
+
 
         int take = v[i][2] + solve(idx,v);
         int skip = solve(i+1,v); 

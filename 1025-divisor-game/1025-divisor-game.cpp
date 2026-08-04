f2c282e6e2375158a6db576ef13081181864f805
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-08-04 18:33:10 +0530

    Time: 0 ms (100%), Space: 7.7 MB (90.22%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,24 +1,6 @@
 class Solution {
 public:
-    int dp[1001];
-
-    bool solve(int n) {
-        if (n == 1) return false;
-
-        if (dp[n] != -1) return dp[n];
-
-        for (int x = 1; x < n; x++) {
-            if (n % x == 0) {
-                if (!solve(n - x))
-                    return dp[n] = true;
-            }
-        }
-
-        return dp[n] = false;
-    }
-
     bool divisorGame(int n) {
-        memset(dp,-1,sizeof(dp));
-        return solve(n);
+        return n%2==0;
     }
 };

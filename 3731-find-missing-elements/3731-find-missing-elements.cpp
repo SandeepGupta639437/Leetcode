5838b88936d9a72b65d0589a51f57048486dea89
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-08-04 17:26:38 +0530

    Time: 0 ms (100%), Space: 34 MB (5.29%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -3,11 +3,10 @@ public:
     vector<int> findMissingElements(vector<int>& nums) {
         int mini = *min_element(begin(nums),end(nums));
         int maxi = *max_element(begin(nums),end(nums));
-        map<int,int>mp;
-        for(int it:nums)mp[it] = 1;
+        set<int>st(nums.begin(),nums.end());
         vector<int>ans;
         for(int i=mini;i<=maxi;i++){
-            if(!mp[i])ans.push_back(i);
+            if(st.count(i)==0)ans.push_back(i);
         }
         return ans;
     }

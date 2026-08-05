95ad9c5d40ab6523013133a5862ec6b6c0e14b43
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-08-05 23:20:04 +0530

    Time: 0 ms (100%), Space: 9 MB (79.28%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int n,m;
+    int dp[21][21];
 
     bool solve(int i,int j,string &s,string &p){
 
@@ -9,18 +10,21 @@ public:
 
         if(j==m) return false;
 
+        if(dp[i][j]!=-1)return dp[i][j];
+
         bool firstMatch = (i<n &&  (s[i]==p[j] || p[j]=='.'));
 
         if(j+1<m && p[j+1]=='*'){
-            return solve(i,j+2,s,p) || (firstMatch && solve(i+1,j,s,p));
+            return dp[i][j] = solve(i,j+2,s,p) || (firstMatch && solve(i+1,j,s,p));
         }
 
-        return firstMatch && solve(i+1,j+1,s,p);
+        return dp[i][j] = firstMatch && solve(i+1,j+1,s,p);
     }
 
     bool isMatch(string s, string p) {
         n=s.size();
         m=p.size();
+        memset(dp,-1,sizeof(dp));
 
         return solve(0,0,s,p);
     }

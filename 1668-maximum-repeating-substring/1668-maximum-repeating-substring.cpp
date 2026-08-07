9c35c3659cd587e743e0a46bc2b5268486bf19ac
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-08-07 12:20:48 +0530

    Time: 0 ms (100%), Space: 9.2 MB (22.46%) - LeetHub

--- 1668-maximum-repeating-substring/1668-maximum-repeating-substring.cpp
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     int maxRepeating(string sequence, string word) {
-        string word1 = word;
-        int i=0;
-        while(true){
-            if(sequence.find(word1)!=string::npos){
-                word1+=word;
-                i++;
-            }else return i;
+
+        int n = sequence.size();
+        int m = word.size();
+
+        vector<int> dp(n,0);
+
+        int ans = 0;
+
+        for(int i=m-1;i<n;i++){
+
+            if(sequence.substr(i-m+1,m)==word){
+
+                dp[i]=1;
+
+                if(i>=m)
+                    dp[i]+=dp[i-m];
+
+                ans=max(ans,dp[i]);
+            }
         }
-        return i;
+
+        return ans;
     }
 };

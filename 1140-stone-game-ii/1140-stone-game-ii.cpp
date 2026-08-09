cf74b5fafc2648c7832ba16f011cd3d5427bb5c1
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-08-09 14:33:42 +0530

    Time: 8 ms (65.33%), Space: 11.1 MB (91.02%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    int dp[101][101];
+    int dp[101][101][2];
     int n;
-    int solve(int i, vector<int>& piles, int M) {
+    int solve(int turn,int i, vector<int>& piles, int M) {
 
         if (i > n) return 0;
 
-        if(dp[i][M]!=-1)return dp[i][M];
+        if(dp[i][M][turn]!=-1)return dp[i][M][turn];
 
         int ans = 0;
         int sum = 0;
+        if (turn == 1)
+            ans = 0;       // Alice wants maximum
+        else
+            ans = 1e9;     // Bob wants minimum
 
         for (int x = 1; x <= 2 * M && i + x - 1 <= n; x++) {
 
             sum += piles[i + x - 1];
 
-            int opponent = solve( i + x, piles, max(M, x) );
-
-            // Total stones remaining
-            int total = 0;
-            for (int k = i; k <= n; k++) total += piles[k];
-
-            // My stones = total - opponent
-            ans = max(ans, total - opponent);
+            if(turn==1){
+                ans = max(ans,sum+solve(0,i+x,piles,max(x,M)));
+            }else{
+                ans = min(ans,solve(1,i+x,piles,max(x,M)));
+            }
         }
 
-        return dp[i][M] = ans;
+        return dp[i][M][turn] = ans;
     }
 
     int stoneGameII(vector<int>& piles) {
@@ -33,6 +34,6 @@ public:
         n = piles.size()-1;
         memset(dp,-1,sizeof(dp));
 
-        return solve(0, piles, 1);
+        return solve(1,0, piles, 1);
     }
 };

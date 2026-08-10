03392aa0e74b9382c8d2b95b94445aae75d9c70a
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-08-10 19:58:51 +0530

    Time: 3 ms (82.68%), Space: 13.3 MB (51.27%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -0,0 +1,102 @@
+class MatrixExpo {
+public:
+    using ll = long long;
+    const ll MOD = 1e9 + 7;
+
+    using Matrix = vector<vector<ll>>;
+
+    Matrix Multiply(const Matrix& A, const Matrix& B) {
+        int n = A.size();
+
+        Matrix C(n, vector<ll>(n, 0));
+
+        for(int i = 0; i < n; i++) {
+            for(int k = 0; k < n; k++) {
+                if(A[i][k] == 0) continue;
+
+                for(int j = 0; j < n; j++) {
+                    if(B[k][j] == 0) continue;
+
+                    C[i][j] =
+                        (C[i][j] + A[i][k] * B[k][j]) % MOD;
+                }
+            }
+        }
+
+        return C;
+    }
+
+    Matrix Identity(int n) {
+        Matrix I(n, vector<ll>(n, 0));
+
+        for(int i = 0; i < n; i++)
+            I[i][i] = 1;
+
+        return I;
+    }
+
+    Matrix Power(const Matrix& A, ll b) {
+        if(b == 0)
+            return Identity(A.size());
+
+        if(b & 1)
+            return Multiply(
+                A,
+                Power(Multiply(A, A), b >> 1)
+            );
+
+        return Power(Multiply(A, A), b >> 1);
+    }
+
+    vector<ll> multiplyMatVec(
+        const Matrix& A,
+        const vector<ll>& v
+    ) {
+        int n = A.size();
+
+        vector<ll> result(n, 0);
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                result[i] =
+                    (result[i] + A[i][j] * v[j]) % MOD;
+            }
+        }
+
+        return result;
+    }
+};
+
+
+class Solution {
+public:
+    using ll = long long;
+    const ll MOD = 1e9 + 7;
+
+    using Matrix = vector<vector<ll>>;
+
+    int countVowelPermutation(int n) {
+
+        Matrix T = {
+            {0, 1, 1, 0, 1},
+            {1, 0, 1, 0, 0},
+            {0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0},
+            {0, 0, 1, 1, 0}
+        };
+
+        MatrixExpo matrixExpo;
+
+        Matrix result = matrixExpo.Power(T, n - 1);
+
+        vector<ll> ans = matrixExpo.multiplyMatVec( result,{1, 1, 1, 1, 1} );
+
+        ll total = 0;
+
+        for(int i = 0; i < 5; i++) {
+            total = (total + ans[i]) % MOD;
+        }
+
+        return total;
+    }
+};

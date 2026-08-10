76bbc48dc9e265584f896b1fee6b0add5d52ad66
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-08-10 20:21:00 +0530

    Time: 1296 ms (5.07%), Space: 13.7 MB (51.27%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -75,28 +75,77 @@ public:
 
     using Matrix = vector<vector<ll>>;
 
-    int countVowelPermutation(int n) {
+    int dp[20001][5];
 
-        Matrix T = {
-            {0, 1, 1, 0, 1},
-            {1, 0, 1, 0, 0},
-            {0, 1, 0, 1, 0},
-            {0, 0, 1, 0, 0},
-            {0, 0, 1, 1, 0}
-        };
+    map<char, int> mp = {
+        {'a', 0},
+        {'e', 1},
+        {'i', 2},
+        {'o', 3},
+        {'u', 4}
+    };
 
-        MatrixExpo matrixExpo;
+    ll solve(int n,char ch){
+        if(n==0)return 1;
+        if(n<0)return 0;
+        ll ans =0;
 
-        Matrix result = matrixExpo.Power(T, n - 1);
+        if(dp[n][mp[ch]]!=-1)return dp[n][mp[ch]];
 
-        vector<ll> ans = matrixExpo.multiplyMatVec( result,{1, 1, 1, 1, 1} );
-
-        ll total = 0;
-
-        for(int i = 0; i < 5; i++) {
-            total = (total + ans[i]) % MOD;
+        if(ch=='a'){
+        ans = (ans+solve(n-1,'e'))%MOD;
+        }
+        if(ch == 'e'){
+            ans = (ans+solve(n-1,'a'))%MOD;
+            ans = (ans+solve(n-1,'i'))%MOD;
+        }
+        if(ch == 'i'){
+            ans = (ans+solve(n-1,'a'))%MOD;
+            ans = (ans+solve(n-1,'e'))%MOD;
+            ans = (ans+solve(n-1,'o'))%MOD;
+            ans = (ans+solve(n-1,'u'))%MOD;
+        }
+        if(ch == 'o'){
+            ans = (ans+solve(n-1,'u'))%MOD;
+            ans = (ans+solve(n-1,'i'))%MOD;
+        }
+        if(ch == 'u'){
+            ans = (ans+solve(n-1,'a'))%MOD;
         }
 
-        return total;
+        return dp[n][mp[ch]] = ans;
+    }
+
+    int countVowelPermutation(int n) {
+
+        // Matrix T = {
+        //     {0, 1, 1, 0, 1},
+        //     {1, 0, 1, 0, 0},
+        //     {0, 1, 0, 1, 0},
+        //     {0, 0, 1, 0, 0},
+        //     {0, 0, 1, 1, 0}
+        // };
+
+        // MatrixExpo matrixExpo;
+
+        // Matrix result = matrixExpo.Power(T, n - 1);
+
+        // vector<ll> ans = matrixExpo.multiplyMatVec( result,{1, 1, 1, 1, 1} );
+
+        // ll total = 0;
+
+        // for(int i = 0; i < 5; i++) {
+        //     total = (total + ans[i]) % MOD;
+        // }
+
+        // return total;
+        memset(dp,-1,sizeof(dp));
+        ll result = 0;
+        result = (result+ solve(n-1,'a'))%MOD;
+        result = (result+ solve(n-1,'e'))%MOD;
+        result = (result+ solve(n-1,'i'))%MOD;
+        result = (result+ solve(n-1,'o'))%MOD;
+        result = (result+ solve(n-1,'u'))%MOD;
+        return result;
     }
 };

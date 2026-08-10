c461ebe5f3ee88bd6ca6977339bf7152578e8921
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-08-10 19:31:12 +0530

    Time: 3 ms (19.04%), Space: 11.2 MB (24.22%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    int solve(long long n){
-        if(n==1)return 0;
+    unordered_map<long long, int> dp;
+
+    int solve(long long n) {
+        if(n == 1)return 0;
+
+        if(dp.count(n)) return dp[n];
+
         int result = INT_MAX;
-        if(n%2){
-            result = min(result,1+solve(n+1));
-            result = min(result,1+solve(n-1));
-        }else{
-            result = min(result,1+solve(n/2));
+
+        if(n % 2 == 0) {
+            result = 1 + solve(n / 2);
+        }
+        else {
+             result = min( 1 + solve(n - 1), 1 + solve(n + 1));
         }
 
-        return result;
+        return dp[n] = result;
     }
+
     int integerReplacement(int n) {
         return solve(n);
     }

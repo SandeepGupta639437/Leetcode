d3e776d0bc297a06fdfecd5b2c0a0cf4f331d856
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-08-24 01:12:44 +0530

    Time: 275 ms (9.69%), Space: 29 MB (92.49%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-  vector<vector<int>>result;
-    void twoSum(vector<int>& nums,int target,int i,int j){
-       while(i<j){
-        if(nums[i]+nums[j]>target)j--;
-        else if(nums[i]+nums[j]<target)i++;
-        else{
-            while(i<j && nums[i]==nums[i+1])i++;
-            while(i < j && nums[j] == nums[j-1]) j--;
-            result.push_back({-target,nums[i],nums[j]});
-            i++;
-            j--;
-        }
-       }
-    }
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>result;
         int n=nums.size();
-        if(n<3)return {};
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1])continue;
 
-            int n1=nums[i];
-            int target=-n1;
-            twoSum(nums,target,i+1,n-1);
+        if(n<3)return {};
+
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<n-1;i++){
+            if (i>0 && nums[i] == nums[i-1]) continue; // to remove duplicacy
+
+            for(int j = i+1;j<n;j++){
+                 if (j>i+1 && nums[j] == nums[j-1]) continue; // to remove duplicasy
+
+                int target = -1*(nums[i]+nums[j]);
+                int l = j + 1, r = n - 1;
+
+                while(l<=r){
+                    int mid = (l+r)/2;
+                    if(nums[mid]==target){
+                        result.push_back({nums[i],nums[j],nums[mid]});
+                        break;
+                    }else if(nums[mid]>target){
+                        r = mid -1;
+                    }else{
+                        l = mid+1;
+                    }
+                }
+
+            }
         }
         return result;
    }

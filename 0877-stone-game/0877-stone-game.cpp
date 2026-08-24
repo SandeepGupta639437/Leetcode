ce2a571e24daec5b3a309e1e629082944f786832
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-08-24 12:54:01 +0530

    Time: 10 ms (23.9%), Space: 11.6 MB (28.03%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
+    int dp[501][501];
+    int solve(vector<int>& piles,int i,int j){
+        if(i==j)return piles[i];
+
+        if(dp[i][j]!=-1)return dp[i][j];
+
+        int takeLeft = piles[i] - solve(piles,i+1,j);
+        int takeRight = piles[j] - solve(piles,i,j-1);
+
+        return dp[i][j] = max(takeLeft,takeRight);
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(piles,0,n-1);
     }
 };

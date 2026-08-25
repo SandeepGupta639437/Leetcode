aaca5a632ab561c762ab7b19fa170369d1853e89
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-08-26 01:10:10 +0530

    Time: 0 ms (100%), Space: 24.6 MB (56.02%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int>exists(101,0);
-        for(int &it:nums){
-            exists[it]=1;
-        }
-        int i;
-        for(i=k;i<=100;i+=k){
-            if(exists[i]==0)return i;
-        }
+        int n = nums.size();vector<int>exists(101,0);for(int &it:nums)exists[it]=1;int i; for(i=k;i<=100;i+=k) if(exists[i]==0)return i;
         return i;
     }
 };

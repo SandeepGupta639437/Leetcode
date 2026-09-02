4aeab41d94e3163372c3731196ca04fb18d11fd6
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-09-02 15:08:27 +0530

    Time: 76 ms (81.41%), Space: 44.3 MB (69.39%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
-    const int INF = 1e9;
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int,int>>>adj(n+1);
 
-    int dijkstra(int source, vector<vector<int>>& times, int n){
-        
-        vector<vector<pair<int,int>>> adj(n+1);
-        for(auto &t : times){
-            int u = t[0], v = t[1], w = t[2];
-            adj[u].push_back({v, w});
+        for(auto &v : times){
+            adj[v[0]].push_back({v[1],v[2]});
         }
-        vector<int> dist(n+1, INF);
-        set<pair<int,int>> st;
 
-        dist[source] = 0;
-        st.insert({0, source});
+        queue<pair<int,int>> que;
 
-        while(!st.empty()){
-            auto [d, v] = *st.begin();
-            st.erase(st.begin());
+        vector<int> timeTaken(n+1,INT_MAX);
+        que.push({0,k});
+        timeTaken[k] = 0;
 
-            for(auto &child : adj[v]){
-                int child_v = child.first;
-                int wt = child.second;
+        while(!que.empty()){
+            auto x = que.front();
+            que.pop();
 
-                if(d + wt < dist[child_v]){
-                    dist[child_v] = d + wt;
-                    st.insert({dist[child_v], child_v});
+            int timeT = x.first;
+            int node = x.second;
+
+            for(auto &v : adj[node]){
+                int nextNode = v.first;
+                int nextTime = timeT + v.second;
+
+                if(nextTime < timeTaken[nextNode]){
+                    timeTaken[nextNode] = nextTime;
+                    que.push({nextTime,nextNode});
                 }
             }
         }
-        int ans = 0;
-        for(int i = 1; i <= n; i++){
-            if(dist[i] == INF) return -1;
-            ans = max(ans, dist[i]);
-        }
+
+        int ans = *max_element(timeTaken.begin()+1,timeTaken.end());
+
+        if(ans == INT_MAX)return -1;
 
         return ans;
-    }
-
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        return dijkstra(k, times, n);
     }
 };

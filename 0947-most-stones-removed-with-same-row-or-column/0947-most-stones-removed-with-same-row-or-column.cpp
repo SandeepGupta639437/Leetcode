234316b7d6059224e0d7584ef253199fb8de1aa4
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-09-02 22:11:09 +0530

    Time: 11 ms (83.03%), Space: 34.8 MB (5.08%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,54 +1,69 @@
-class Solution {
+class DSU {
 public:
-    void dfs(vector<vector<int>>& stones,int index, vector<bool>&visited){
-        visited[index]=true;
+    vector<int> parent;
+    vector<int> sz;
 
-        for(int i=0;i<stones.size();i++){
-            if(visited[i]==false && ((stones[i][0]==stones[index][0])||(stones[i][1]==stones[index][1]))){
-               dfs(stones,i,visited);
-            }
+    DSU(int n) {
+        parent.resize(n);
+        sz.resize(n, 1);
+
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
         }
     }
 
+    int find(int v) {
+        if(parent[v] == v) return v;
+        return parent[v] = find(parent[v]);
+    }
+
+    void Union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if(a == b) return;
+
+        if(sz[a] < sz[b]) swap(a, b);
+
+        parent[b] = a;
+        sz[a] += sz[b];
+    }
+};
+
+class Solution {
+public:
     int removeStones(vector<vector<int>>& stones) {
-      int n=stones.size();
-      
-      vector<bool>visited(n,false);
 
-      int group=0;
+        int OFFSET = 10001;
 
-      for(int i=0;i<n;i++){
-        if(visited[i]==true)continue;
-        else{
-            dfs(stones,i,visited);
-            group++;
+        // 0 ... 10000        -> rows
+        // 10001 ... 20001    -> columns
+        DSU dsu(20002);
+
+        // Initially we have 20002 components,
+        // but we only care about nodes that actually occur.
+        
+        unordered_set<int> used;
+
+        for(auto &stone : stones) {
+            int x = stone[0];
+            int y = stone[1] + OFFSET;
+
+            dsu.Union(x, y);
+
+            used.insert(x);
+            used.insert(y);
         }
-      }
-      return n-group;
 
+        // Count components among only used row/column nodes
+        unordered_set<int> roots;
 
+        for(int node : used) {
+            roots.insert(dsu.find(node));
+        }
 
+        int components = roots.size();
 
-
-    //   map<int,int>mppr;
-    //   map<int,int>mppc;
-    //   for(int i=0;i<n;i++){
-    //     mppr[stones[i][0]]=mppr[stones[i][0]]=1;
-    //     mppc[stones[i][0]]=mppc[stones[i][0]]=1;
-    //   }
-    //   int left=0;
-    //   for(int i=0;i<n;i++){
-    //     if(mppr[stones[i][0]]==2 || mppc[stones[i][1]]==2){
-    //        mppr[stones[i][0]]=2;
-    //        mppc[stones[i][1]]=2;
-    //        continue;
-    //     }
-    //     else{
-    //         left++;
-    //         mppr[stones[i][0]]=2;
-    //         mppc[stones[i][1]]=2;
-    //     }
-    //   }
-    //   return n-left;
+        return stones.size() - components;
     }
 };

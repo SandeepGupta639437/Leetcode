4e05701dffa716c1141566342150ac42abf70087
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-09-02 19:53:12 +0530

    Time: 0 ms (100%), Space: 42.1 MB (91.8%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,56 @@
+class DSU{
+public:
+    vector<int>parent;
+    vector<int>sz;
+
+    DSU(int n){
+        parent.resize(n);
+        sz.resize(n);
+    }
+
+    void make(int v){
+        parent[v] = v;
+        sz[v] = 1;
+    }
+
+    int find(int v){
+        if(parent[v] == v)return v;
+        return parent[v] = find(parent[v]);
+    }
+
+    void Union(int a,int b){
+        a = find(a);
+        b = find(b);
+
+        if(a!=b){
+            if(sz[a]<sz[b])swap(a,b);
+            parent[b] =a;
+            sz[a] += sz[b];
+        }
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DSU dsu(n);
+        for(int i=0;i<n;i++){
+            dsu.make(i);
+        }
+
+        int components = n;
+
+        for(auto &v : connections) {
+
+            int u = v[0];
+            int w = v[1];
+
+            if(dsu.find(u) != dsu.find(w)) {
+                dsu.Union(u, w);
+                components--;
+            }
+        }
+
+        return (connections.size()<n-1)?-1 : components-1;
+    }
+};

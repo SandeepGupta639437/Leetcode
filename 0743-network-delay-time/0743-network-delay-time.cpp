61d9223a0e9c4300c36830c1cc80ab2e23cf4374
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-09-02 15:10:45 +0530

    Time: 84 ms (53.78%), Space: 44.3 MB (83.62%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>>adj(n+1);
+        vector<vector<pair<int,int>>> adj(n + 1);
 
-        for(auto &v : times){
-            adj[v[0]].push_back({v[1],v[2]});
+        for(auto &v : times) {
+            adj[v[0]].push_back({v[1], v[2]});
         }
 
-        queue<pair<int,int>> que;
+        priority_queue<
+            pair<int,int>,
+            vector<pair<int,int>>,
+            greater<pair<int,int>>
+        > pq;
 
-        vector<int> timeTaken(n+1,INT_MAX);
-        que.push({0,k});
-        timeTaken[k] = 0;
+        vector<int> dist(n + 1, INT_MAX);
 
-        while(!que.empty()){
-            auto x = que.front();
-            que.pop();
+        dist[k] = 0;
+        pq.push({0, k});
 
-            int timeT = x.first;
-            int node = x.second;
+        while(!pq.empty()) {
+            auto [time, node] = pq.top();
+            pq.pop();
 
-            for(auto &v : adj[node]){
-                int nextNode = v.first;
-                int nextTime = timeT + v.second;
+            if(time > dist[node])
+                continue;
 
-                if(nextTime < timeTaken[nextNode]){
-                    timeTaken[nextNode] = nextTime;
-                    que.push({nextTime,nextNode});
+            for(auto &[nextNode, edgeTime] : adj[node]) {
+                int nextTime = time + edgeTime;
+
+                if(nextTime < dist[nextNode]) {
+                    dist[nextNode] = nextTime;
+                    pq.push({nextTime, nextNode});
                 }
             }
         }
 
-        int ans = *max_element(timeTaken.begin()+1,timeTaken.end());
+        int ans = *max_element(dist.begin() + 1, dist.end());
 
-        if(ans == INT_MAX)return -1;
-
-        return ans;
+        return ans == INT_MAX ? -1 : ans;
     }
 };

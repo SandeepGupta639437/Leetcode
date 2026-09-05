a51aa08207e80c839aa3089f4f4434949d333316
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-09-06 00:19:33 +0530

    Time: 6 ms (99.81%), Space: 30.7 MB (96.46%) - LeetHub

--- 4035-maximum-valid-split-positions-i/4035-maximum-valid-split-positions-i.cpp
+++ 4035-maximum-valid-split-positions-i/4035-maximum-valid-split-positions-i.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    int solve(vector<int> &nums,int k){
+    int solve(vector<int> &nums,int k,vector<int> &suffix,vector<int> &prefix){
         int n = nums.size();
 
-        vector<int>arr;
-        for(int i=0;i<n;i++){
-            if(k!=i)arr.push_back(nums[i]);
+        int removed ;
+
+        // 1. Remove nums[k]
+        if(k != -1) {
+            removed = nums[k];
+            nums.erase(nums.begin() + k);
         }
 
-        int m = arr.size();
+        int m = nums.size();
 
-        if(m == 0) return 0;
+        if(m == 0) {
+            nums.insert(nums.begin() + k, removed);
+            return 0;
+        }
 
-        vector<int>prefix(m,0);
-        vector<int>suffix(m,0);
+        prefix.resize(m,0);
+        suffix.resize(m,0);
 
-        prefix[0] = arr[0];
+        prefix[0] = nums[0];
 
         for(int i=1;i<m;i++){
-            prefix[i] = __gcd(prefix[i-1],arr[i]);
+            prefix[i] = __gcd(prefix[i-1],nums[i]);
         }
 
-        suffix[m-1] = arr[m-1];
+        suffix[m-1] = nums[m-1];
         for(int i=m-2;i>=0;i--){
-            suffix[i] = __gcd(suffix[i+1],arr[i]);
+            suffix[i] = __gcd(suffix[i+1],nums[i]);
         }
 
         int ans = 0;
@@ -33,19 +39,33 @@ public:
                 ans++;
             }
         }
+        if(k != -1) {
+            nums.insert(nums.begin() + k, removed);
+        }
         return ans;
     }
+
 
     int maxValidSplits(vector<int>& nums) {
 
         int n = nums.size();
 
         int ans = 0;
+        vector<int>suffix;
+        vector<int>prefix;
+        vector<int> prefixmain(n, 0);
+        prefixmain[0] = nums[0];
+        for (int i=1;i<n;i++){
+            prefixmain[i] = gcd(prefixmain[i-1],nums[i]);
+        }
         // case1
-        ans = solve(nums,-1); //no removal
+        ans = solve(nums,-1,suffix,prefix); //no removal     O(n)
 
         for(int i=0;i<n;i++){
-            int curr = solve(nums,i);
+            if (i > 0 && prefixmain[i] == prefixmain[i - 1]) continue;
+             // remove krne se koi fayda nhi hone wala 
+            //ye number of solve calls ko bhut decrease krde rha hai
+            int curr = solve(nums,i,suffix,prefix);
             ans = max(curr,ans);
         }
 

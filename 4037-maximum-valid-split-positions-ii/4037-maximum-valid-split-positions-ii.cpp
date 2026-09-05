a0c159c4f32600f7d9cb6994c91d2a2f09d570f8
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-09-06 00:13:28 +0530

    Time: 152 ms (87.53%), Space: 326.4 MB (54.65%) - LeetHub

--- 4037-maximum-valid-split-positions-ii/4037-maximum-valid-split-positions-ii.cpp
+++ 4037-maximum-valid-split-positions-ii/4037-maximum-valid-split-positions-ii.cpp
@@ -1,49 +1,62 @@
 class Solution {
 public:
-    int solve(vector<int>& prefix, vector<int>& suffix, int skip,vector<int>&a) {
-        int n = a.size();
+    int solve(vector<int> &nums,int k){
+        int n = nums.size();
 
-        for (int i = 1; i <= n; i++) {
-            if (i - 1 == skip) {
-                prefix[i] = prefix[i - 1];
-                continue;
-            }
-            prefix[i] = gcd(prefix[i - 1], a[i - 1]);
+        vector<int>arr;
+        for(int i=0;i<n;i++){
+            if(k!=i)arr.push_back(nums[i]);
         }
-        for (int i = n - 1; i >= 0; i--) {
-            if (i == skip) {
-                suffix[i] = suffix[i + 1];
-                continue;
-            }
-            suffix[i] = gcd(suffix[i + 1], a[i]);
-        }
-        
-        int curr = 0;
-        for (int i = 0; i < n - 1; i++) {
-            if (i == skip)  continue;
 
-            if (prefix[i + 1] == suffix[i + 1]) curr++;
+        int m = arr.size();
+
+        if(m == 0) return 0;
+
+        vector<int>prefix(m,0);
+        vector<int>suffix(m,0);
+
+        prefix[0] = arr[0];
+
+        for(int i=1;i<m;i++){
+            prefix[i] = __gcd(prefix[i-1],arr[i]);
         }
-        return curr;
+
+        suffix[m-1] = arr[m-1];
+        for(int i=m-2;i>=0;i--){
+            suffix[i] = __gcd(suffix[i+1],arr[i]);
+        }
+
+        int ans = 0;
+
+        for(int i=0;i<m-1;i++){
+            if(prefix[i]==suffix[i+1]){
+                ans++;
+            }
+        }
+        return ans;
     }
-    int maxValidSplits(vector<int>& a) {
 
-        int n = a.size(), ans = 0;
+    int maxValidSplits(vector<int>& nums) {
 
-        vector<int> prefixmain(n + 1, 0);
-        for (int i = 1; i <= n; i++){
-            prefixmain[i] = gcd(prefixmain[i - 1], a[i - 1]);
+        int n = nums.size();
+
+        int ans = 0;
+        vector<int> prefixmain(n, 0);
+        prefixmain[0] = nums[0];
+        for (int i=1;i<n;i++){
+            prefixmain[i] = gcd(prefixmain[i-1],nums[i]);
         }
+        // case1
+        ans = solve(nums,-1); //no removal
 
-        for (int i = 0; i <= n; i++) {
-            if (i > 0 && prefixmain[i] == prefixmain[i - 1]) continue; // remove krne se koi fayda nhi hone wala 
+        for(int i=0;i<n;i++){
+            if (i > 0 && prefixmain[i] == prefixmain[i - 1]) continue;
+             // remove krne se koi fayda nhi hone wala 
             //ye number of solve calls ko bhut decrease krde rha hai
-
-            int skip = i - 1;
-            vector<int> prefix(n + 1, 0);
-            vector<int> suffix(n + 1, 0);
-            ans = max(ans, solve(prefix, suffix, i - 1,a));
+            int curr = solve(nums,i);
+            ans = max(curr,ans);
         }
+
         return ans;
     }
 };

84e64633c8c2934fd0c7cc531e25fded682f5bd2
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-09-06 19:11:11 +0530

    Time: 5 ms (58.55%), Space: 202.8 MB (82.22%) - LeetHub

--- 4045-count-robot-groups/4045-count-robot-groups.cpp
+++ 4045-count-robot-groups/4045-count-robot-groups.cpp
@@ -1,43 +1,20 @@
 class Solution {
 public:
     int countGroups(vector<int>& position, vector<int>& speed, int distance) {
-        //for t0
-        stack<pair<int,int>>st;
         int n = position.size();
-        st.push({position[0],speed[0]});
-
-        for(int i=1;i<n;i++){
-            auto [x,y] = st.top();
-            if(position[i]-x <= distance){
-                st.pop();
+        int groups = 1;
+        int l = n-1;
+        int r = n-1;
+        for(int i=n-2;i>=0;i--){
+            if(position[l]-position[i] <= distance || speed[i]>speed[r]){
+                l=i;
             }
-            st.push({position[i],speed[i]});
-        }
-
-        vector<pair<int,int>>afterT;
-        while(!st.empty()){
-            afterT.push_back(st.top());
-            st.pop();
-        }
-
-        reverse(begin(afterT),end(afterT));
-
-        stack<pair<int,int>>finalSt;
-        if(afterT.size()>0)finalSt.push(afterT[0]);
-
-        for(int i=1;i<afterT.size();i++){
-            auto [x,y] = afterT[i];
-            auto [X,Y] = finalSt.top();
-
-            if(Y>y){
-                finalSt.pop();
+            else{
+                groups++;
+                l = i;
+                r=i;
             }
-            while(!finalSt.empty() && finalSt.top().second > y){
-                finalSt.pop();
-            }
-            finalSt.push({x,y});
-            
         }
-        return finalSt.size();
+        return groups;
     }
 };

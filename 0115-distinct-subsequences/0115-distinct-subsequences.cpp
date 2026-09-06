f5a7d6c72eee77f2444f1734e1e02bcf3c8ad08f
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-09-06 12:36:06 +0530

    Time: 31 ms (53.36%), Space: 12.9 MB (80.54%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -6,13 +6,16 @@ public:
     int solve(int i,int j,string &s,string &t){
 
         if(j==m) return dp[i][j] = 1;
-
         if(i==n) return dp[i][j] = 0;
+        
         if(dp[i][j]!=-1)return dp[i][j];
 
-        if(s[i]==t[j]) return dp[i][j] = solve(i+1,j+1,s,t) + solve(i+1,j,s,t);
+        int ans = 0;
 
-        return dp[i][j] = solve(i+1,j,s,t);
+        if(s[i]==t[j])ans +=solve(i+1,j+1,s,t);
+        ans += solve(i+1,j,s,t);
+
+        return dp[i][j] = ans;        
     }
 
     int numDistinct(string s, string t) {

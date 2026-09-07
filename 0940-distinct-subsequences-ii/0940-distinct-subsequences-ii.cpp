d73b89c079f319dbb31693746bc4df7fa45f0747
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-09-08 01:47:55 +0530

    Time: 3 ms (37.83%), Space: 10.6 MB (8.94%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -1,18 +1,54 @@
 class Solution {
-    static constexpr int MOD = 1e9 + 7;
-
 public:
-    int distinctSubseqII(string s) {
-        int tot = 0, dp[26]{};
+    int M = 1e9+7;
+    int dp[2001];
+    vector<int>prev;
 
-        for (auto& c : s) {
-            c -= 'a';
-            int add = (tot - dp[c] + MOD) % MOD;
-            dp[c] = 1 + tot;
-            tot = (dp[c] + add) % MOD;
+    int solve(int n){
+        if(n==0)return 1;
+        if(dp[n]!=-1)return dp[n];
+
+        int total = ((1LL*2*solve(n-1))%M);
+
+        if(prev[n] != 0){
+            int duplicates = solve(prev[n]-1);
+            total = (total-duplicates+M)%M;
         }
 
-        return tot;
+        return dp[n] = total;
+    }
+    int distinctSubseqII(string s) {
+        int n = s.length();
+        memset(dp,-1,sizeof(dp));
+        prev.assign(n+1,0);
+
+        vector<int>lastSeen(26,0);
+        for(int i=1;i<=n;i++){
+            int idx = s[i-1] - 'a';
+            prev[i] = lastSeen[idx] ;
+            lastSeen[idx] = i; 
+        }
+        return (solve(n)-1+M)%M;
     }
 };
-// https://leetcode.com/problems/distinct-subsequences-ii/solutions/8506448/solution-by-la_castille-zs7g
+
+
+
+// class Solution {
+//     static constexpr int MOD = 1e9 + 7;
+
+// public:
+//     int distinctSubseqII(string s) {
+//         int tot = 0, dp[26]{};
+
+//         for (auto& c : s) {
+//             c -= 'a';
+//             int add = (tot - dp[c] + MOD) % MOD;
+//             dp[c] = 1 + tot;
+//             tot = (dp[c] + add) % MOD;
+//         }
+
+//         return tot;
+//     }
+// };
+// // https://leetcode.com/problems/distinct-subsequences-ii/solutions/8506448/solution-by-la_castille-zs7g

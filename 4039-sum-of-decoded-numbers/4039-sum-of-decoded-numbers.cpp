a33c29a38ba53dad12d9a6c39853824b5456becc
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-09-09 20:27:52 +0530

    Time: 55 ms (82.69%), Space: 88.8 MB (90.72%) - LeetHub

--- 4039-sum-of-decoded-numbers/4039-sum-of-decoded-numbers.cpp
+++ 4039-sum-of-decoded-numbers/4039-sum-of-decoded-numbers.cpp
@@ -3,14 +3,17 @@ public:
     #define ll long long
     const int M = 1e9+7;
 
-    pair<int, int> findXY(int width, ll d) {
-        string s = to_string(d);
+    // pair<int, int> findXY(int width, ll d) {
+    //     string s = to_string(d);
 
-        string x = s.substr(0, width);
-        string y = s.substr(width);
+    //     string x = s.substr(0, width);
+    //     string y = s.substr(width);
 
-        return {stoi(x), stoi(y)};
-    }
+    //     return {stoi(x), stoi(y)};
+    // }
+
+    static constexpr uint64_t TENS[] = { 1, 10, 100, 1'000, 10'000, 100'000, 1'000'000, 10'000'000, 100'000'000, 1'000'000'000, 10'000'000'000, 100'000'000'000,
+        1'000'000'000'000, 10'000'000'000'000, 100'000'000'000'000, 1'000'000'000'000'000 };
 
     ll modPow(ll a, ll b) {
         if (b == 0) return 1;
@@ -26,9 +29,15 @@ public:
         int sum = 0;
 
         for(long long& it: nums){
-            int width = it % 10;
+            int w = it % 10;
             ll d = it/10;
-            auto [x,y] = findXY(width,d);
+
+            uint8_t o = 0;
+            for ( ; TENS[o] <= d; o++) ;
+
+            const uint64_t t = TENS[o-w];
+
+            const uint64_t x = d / t, y = d % t;
 
             ll powerSum = (ll)modPow(x,y);
             sum = (sum + 1LL*powerSum)%M;

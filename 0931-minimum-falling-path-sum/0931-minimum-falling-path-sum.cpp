a62df5fb0f9a50a6764504fa848b84e4752a0a0e
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-09-11 01:08:51 +0530

    Time: 0 ms (100%), Space: 14.6 MB (85.35%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size();
+    int n;
+    long long dp[101][101];
+    vector<vector<int>>directions = {{1,-1},{1,0},{1,1}};
+    int pathSum(vector<vector<int>>& matrix,int i,int j){
+        if(i == n-1) return matrix[i][j];
 
-        vector<int> next = matrix[n-1];
-        vector<int> curr(n);
+        if(dp[i][j]!=-1e9)return dp[i][j];
 
-        for(int i = n-2; i >= 0; i--) {
-            for(int j = 0; j < n; j++) {
+        int ans = INT_MAX;
+        for(auto &dir: directions ){
+            int newX = i + dir[0];
+            int newY = j + dir[1];
 
-                if(j == 0) {
-                    curr[j] = matrix[i][j] + min(next[j], next[j+1]);
-                }
-                else if(j == n-1) {
-                    curr[j] = matrix[i][j] + min(next[j], next[j-1]);
-                }
-                else {
-                    curr[j] = matrix[i][j] +
-                              min(next[j],
-                              min(next[j-1], next[j+1]));
-                }
-            }
+            if(newX>=n || newX<0 || newY>=n || newY<0)continue;
 
-            next.swap(curr);
+            ans = min(ans,matrix[i][j] + pathSum(matrix,newX,newY));
         }
 
-        return *min_element(next.begin(), next.end());
+        return dp[i][j] = ans;
+    }
+
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int ans = INT_MAX;
+        n = matrix.size();
+        fill(&dp[0][0], &dp[0][0] + 101 * 101, -1e9);
+        for(int j=0;j<n;j++){
+            ans = min(ans,pathSum(matrix,0,j));
+        }
+        return ans;
     }
 };

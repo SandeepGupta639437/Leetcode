d44ad0c05d988f5ba6a0ec57574baaa9efcc4cd0
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-09-10 11:31:44 +0530

    Time: 7 ms (54.96%), Space: 15.8 MB (29.76%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -11,26 +11,44 @@
  */
 class Solution {
 public:
-    int result = 0;
-    int findSum(TreeNode* root,int& count){
-        if(root==NULL)return 0;
+    // int result = 0;
+    // int findSum(TreeNode* root,int& count){
+    //     if(root==NULL)return 0;
         
-        count++;
-        int leftSum = findSum(root->left,count);
-        int rightSum = findSum(root->right,count);
+    //     count++;
+    //     int leftSum = findSum(root->left,count);
+    //     int rightSum = findSum(root->right,count);
 
-        return leftSum+rightSum+root->val;
-    }
-    void solve(TreeNode* root){
-        if(root==NULL)return ;
+    //     return leftSum+rightSum+root->val;
+    // }
+    // void solve(TreeNode* root){
+    //     if(root==NULL)return ;
 
-        int count = 0;
-        int sum = findSum(root,count);
+    //     int count = 0;
+    //     int sum = findSum(root,count);
 
-        if(sum/count == root->val)result++;
+    //     if(sum/count == root->val)result++;
 
-        solve(root->left);
-        solve(root->right);
+    //     solve(root->left);
+    //     solve(root->right);
+    // }
+
+    int result = 0;
+
+    pair<int,int> solve(TreeNode* root){
+        if(root==NULL)return {0,0};
+
+        auto p1 = solve(root->left);
+        auto p2 = solve(root->right);
+
+        int totalSum = p1.first + p2.first + root->val;
+        int totalCnt = p1.second + p2.second + 1;
+
+        int avg = (totalSum)/totalCnt;
+
+        if(avg == root->val)result++;
+
+        return {totalSum,totalCnt};
     }
     int averageOfSubtree(TreeNode* root) {
         solve(root);

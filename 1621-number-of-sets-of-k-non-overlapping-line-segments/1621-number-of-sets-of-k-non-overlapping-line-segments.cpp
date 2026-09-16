a1c9ec4b711df4720280f75131725fed19b6596b
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-09-16 07:35:51 +0530

    Time: 42 ms (58.88%), Space: 16.8 MB (53.74%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
     int dp[1001][1001];
+    int prefdp[1001][1001];
     int MOD = 1e9+7;
+    int solve2(int n,int k){
+        if(n<2)return 0;
+        if(prefdp[n][k]!=-1)return prefdp[n][k];
+        return prefdp[n][k] = (solve2(n-1,k)+solve(n,k))%MOD;
+    }
     int solve(int n , int k){
         if(k==1){
             return n*(n-1)/2;
@@ -15,14 +21,15 @@ public:
         int total = 0;
         total = (total + solve(n-1,k))%MOD;
 
-        for(int i=1;i<n;i++){
-            total = (total + solve(n-i,k-1))%MOD;
-        }
+        // for(int i=1;i<n;i++){
+        //     total = (total + solve(n-i,k-1))%MOD;
+        // }
+        total = (total+solve2(n-1,k-1))%MOD;
         return dp[n][k] = total;
     }
     int numberOfSets(int n, int k) {
         memset(dp,-1,sizeof(dp));
-        // memset(prefdp,-1,sizeof(prefdp));
+        memset(prefdp,-1,sizeof(prefdp));
         return solve(n,k);
     }
 };

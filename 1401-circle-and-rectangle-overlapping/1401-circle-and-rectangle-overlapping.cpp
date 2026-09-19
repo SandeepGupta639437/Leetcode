8a297763c04d62f3a0e73013939202d8399b8a25
SANDEEP GUPTA <sandeepguptag18@gmail.com>
2026-09-19 11:58:09 +0530

    Time: 0 ms (100%), Space: 7.9 MB (35%) - LeetHub

--- 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.cpp
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     bool checkOverlap(int r, int x, int y, int x1, int y1, int x2, int y2) {
         
-        int closestX =0;
-        int closestY =0;
+        int closestX =clamp(x,x1,x2);
+        int closestY =clamp(y,y1,y2);
 
-        if(x1>x)closestX=x1;
-        else if(x2<x)closestX=x2;
-        else closestX = x;
+        // if(x1>x)closestX=x1;
+        // else if(x2<x)closestX=x2;
+        // else closestX = x;
 
-        if(y1>y)closestY=y1;
-        else if(y2<y)closestY=y2;
-        else closestY = y;
+        // if(y1>y)closestY=y1;
+        // else if(y2<y)closestY=y2;
+        // else closestY = y;
 
         int dx = x - closestX;
         int dy = y - closestY;
